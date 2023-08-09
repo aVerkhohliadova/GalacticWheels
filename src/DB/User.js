294682450d31d1da71d7a1fe08aecc79ecd0f11a
Alla Verkhohliadova <a_verkhohliadova@fanshaweonline.ca>
2023-08-09 05:49:40 -0400

    profile/orders pages in general

--- src/DB/User.js
+++ src/DB/User.js
@@ -1,9 +1,12 @@
 export default class User {
   // Add functions specific to the ADT in it's own class, then call it from hooks in the page.
   // This is also where we will update data in firebase using apis from api/ folder
-  constructor({ name, id }) {
+  constructor({ name, id, phone, email, password }) {
     this.name = name;
     this.id = id;
+    this.phone = phone;
+    this.email = email;
+    this.password = password;
   }
 
   // signup() {}
